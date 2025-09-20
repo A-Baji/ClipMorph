6dfba452b4cdc8c1d194b7bf2eb2bbfeb24a4c22
A-Baji <bidabaji@gmail.com>
2025-09-19 23:06:25 -0500

    move logging settings to init

--- ClipMorph/__init__.py
+++ ClipMorph/__init__.py
@@ -1,3 +1,26 @@
 import logging
+import warnings
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+# Suppress various library warnings and debug logs BEFORE any heavy imports
+warnings.filterwarnings("ignore", category=UserWarning, module="pyannote")
+warnings.filterwarnings("ignore", category=UserWarning, module="speechbrain")
+warnings.filterwarnings("ignore", category=UserWarning, module="torch")
+
+# Configure root logger first to catch early messages
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+# Suppress specific library loggers
+logging.getLogger("speechbrain").setLevel(logging.ERROR)
+logging.getLogger("pyannote").setLevel(logging.ERROR)
+logging.getLogger("torch").setLevel(logging.ERROR)
+logging.getLogger("speechbrain.utils.torch_audio_backend").setLevel(
+    logging.ERROR)
+logging.getLogger("speechbrain.utils.checkpoints").setLevel(logging.ERROR)
+logging.getLogger("speechbrain.core").setLevel(logging.ERROR)
+logging.getLogger("speechbrain.utils.parameter_transfer").setLevel(
+    logging.ERROR)
+
+# Disable debug messages from speechbrain entirely
+logging.getLogger("speechbrain").propagate = False
